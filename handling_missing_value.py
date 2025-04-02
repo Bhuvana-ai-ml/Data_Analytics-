c93a01d8e3bfc9f2a3c47a426c9f5715c063e3ab
Bhuvana <bhuvana.pbk@gmail.com>
2025-04-02 13:40:34 +0530

    deleted multiple unwanted files

--- handling_missing_value.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv(r"C:\Users\Bhuvana P\Downloads\winequality-red.csv")
-# why r? cause in python '\' means escape character.so, we use r to avoid misinterpret
-df.head
